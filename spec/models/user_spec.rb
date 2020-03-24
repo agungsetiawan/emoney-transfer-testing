61dd36f716783842cb80169d86050ccd5ad56456
Agung Setiawan <com.agungsetiawan@gmail.com>
2020-03-24 09:25:38 +0700

    Refactor to let

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe '#priority_user?' do
+    let(:user) { create(:user) }
+
     it 'return true if balance more than User::PRIORITY_USER_MINIMUM_BALANCE' do
-      user    = create(:user)
       account = create(:account, balance: 200_000, user: user)
 
       expect(user.priority_user?).to eq(true)
     end
 
     it 'return false if balance less than User::PRIORITY_USER_MINIMUM_BALANCE' do
-      user    = create(:user)
       account = create(:account, balance: 50_000, user: user)
 
       expect(user.priority_user?).to eq(false)
