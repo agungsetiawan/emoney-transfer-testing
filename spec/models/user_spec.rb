61633cef14aa209aab66beb588cef0b71e11a2fd
Agung Setiawan <com.agungsetiawan@gmail.com>
2020-03-24 09:37:13 +0700

    Refactor to context

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,16 +4,20 @@ RSpec.describe User, type: :model do
   describe '#priority_user?' do
     let(:user) { create(:user) }
 
-    it 'return true if balance more than User::PRIORITY_USER_MINIMUM_BALANCE' do
-      account = create(:account, balance: 200_000, user: user)
+    context 'balance more than User::PRIORITY_USER_MINIMUM_BALANCE' do
+      it 'return true' do
+        account = create(:account, balance: 200_000, user: user)
 
-      expect(user.priority_user?).to eq(true)
+        expect(user.priority_user?).to eq(true)
+      end
     end
 
-    it 'return false if balance less than User::PRIORITY_USER_MINIMUM_BALANCE' do
-      account = create(:account, balance: 50_000, user: user)
+    context 'balance less than User::PRIORITY_USER_MINIMUM_BALANCE' do
+      it 'return false' do
+        account = create(:account, balance: 50_000, user: user)
 
-      expect(user.priority_user?).to eq(false)
+        expect(user.priority_user?).to eq(false)
+      end
     end
   end
 end
