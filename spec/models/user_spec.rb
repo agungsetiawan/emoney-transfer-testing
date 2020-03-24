ea377734afc110acdec4d122b956122cf2e90de1
Agung Setiawan <com.agungsetiawan@gmail.com>
2020-03-24 09:10:51 +0700

    Create spec for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#priority_user?' do
+    it 'return true if balance more than User::PRIORITY_USER_MINIMUM_BALANCE' do
+      user    = create(:user)
+      account = create(:account, balance: 200_000, user: user)
+
+      expect(user.priority_user?).to eq(true)
+    end
+
+    it 'return false if balance less than User::PRIORITY_USER_MINIMUM_BALANCE' do
+      user    = create(:user)
+      account = create(:account, balance: 50_000, user: user)
+
+      expect(user.priority_user?).to eq(false)
+    end
+  end
 end
