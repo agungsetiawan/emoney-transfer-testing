694b43f5cdb52eb650ab169e629d3e8b6dbdaadd
Agung Setiawan <com.agungsetiawan@gmail.com>
2020-03-24 07:23:06 +0700

    Add priority_user? method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_one :account
+
+  PRIORITY_USER_MINIMUM_BALANCE = 100_000
+
+  def priority_user?
+    account.balance > PRIORITY_USER_MINIMUM_BALANCE
+  end
 end
